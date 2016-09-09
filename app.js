3f0a15b9c7484a699ea7030272b09f670bf4fdd8
Joakim Holm <joakim.holm@adaptiv.se>
2016-09-09 11:27:31 +0200

    Changes hello text

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 8000;
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.end('<h1>Hello World</h1>');
+  response.end('<h1>Hello World from Jocke!</h1>');
 });
 
 server.listen(port, () => console.log(`Listening on port ${port}.`));
